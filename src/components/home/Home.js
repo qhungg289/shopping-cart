b10e5bd2c6c1b2077e1346bcd46e162d4ac2e6c4
Hung Duong Quang <qhungg289@protonmail.com>
2021-11-09 19:51:12 +0700

    Add illustration

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
+import illustration from "../../assets/9.png";
+import "../../style/Home.css";
 
 function Home() {
 	return (
 		<div className="home">
-			<h1>Tap into your imagination</h1>
-			<p>
-				With the very best quality products and exceptional warranty policies.
-				You can put your trust in us for take care of your need for a good
-				smartphone.
-			</p>
-			<Link to="/shop">
-				<button>View our products</button>
-			</Link>
+			<div id="home-content">
+				<h1>Tap into your imagination</h1>
+				<p>
+					With the very best quality products and exceptional warranty policies.
+					You can put your trust in us for take care of your need for a good
+					smartphone.
+				</p>
+				<Link to="/shop">
+					<button>View our products</button>
+				</Link>
+			</div>
+			<img src={illustration} alt="Hand holding a smartphone" />
 		</div>
 	);
 }
