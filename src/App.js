21e916c67eab748b6bfd1ea3a98297fd3e88dcb3
Hung Duong Quang <qhungg289@protonmail.com>
2021-11-09 19:55:03 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./style/App.css";
+import NavBar from "./components/navigation/NavBar";
+import Home from "./components/home/Home";
+import Shop from "./components/shop/Shop";
+import Cart from "./components/shop/Cart";
+import { useState } from "react";
 
 function App() {
-	return <div className="App"></div>;
+	const [selectedItems, setSelectedItems] = useState([]);
+
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<NavBar itemsAmount={selectedItems.length} />
+				<Switch>
+					<Route path="/shop">
+						<Shop />
+					</Route>
+					<Route path="/cart">
+						<Cart selectedItems={selectedItems} />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
