8c903c4b39a99573b975b79aea0c0dac9336a2e0
Hung Duong Quang <qhungg289@protonmail.com>
2021-11-14 19:13:40 +0700

    Update App logic

--- src/App.js
+++ src/App.js
@@ -7,18 +7,68 @@ import Cart from "./components/shop/Cart";
 import { useState } from "react";
 
 function App() {
-	const [selectedItems, setSelectedItems] = useState([]);
+	const [selectedPhones, setSelectedPhones] = useState([]);
+
+	function addPhoneToCart(phone) {
+		if (
+			// If phone didn't already in the state
+			!selectedPhones.some(
+				(prevSelectedPhone) => phone.name === prevSelectedPhone.name
+			)
+		) {
+			setSelectedPhones([...selectedPhones, { ...phone, amount: 1 }]);
+		} else {
+			let tempSelectedPhone = [...selectedPhones]; // clone the state
+			let tempPhone;
+
+			for (let i = 0; i < tempSelectedPhone.length; i++) {
+				if (phone.name === tempSelectedPhone[i].name) {
+					tempPhone = { ...tempSelectedPhone[i] }; // clone the object
+					tempPhone.amount++;
+					tempSelectedPhone[i] = tempPhone;
+					break;
+				}
+			}
+
+			setSelectedPhones(tempSelectedPhone);
+		}
+	}
+
+	function removePhoneFormCart(phone) {
+		let tempSelectedPhone = [...selectedPhones];
+		const index = tempSelectedPhone.indexOf(phone);
+
+		tempSelectedPhone.splice(index, 1);
+
+		setSelectedPhones(tempSelectedPhone);
+	}
+
+	function handleAmountChange(phone, value) {
+		let tempSelectedPhone = [...selectedPhones];
+		let tempPhone;
+		const index = tempSelectedPhone.indexOf(phone);
+
+		tempPhone = { ...tempSelectedPhone[index] };
+		tempPhone.amount = value;
+		tempSelectedPhone[index] = tempPhone;
+
+		setSelectedPhones(tempSelectedPhone);
+	}
 
 	return (
 		<BrowserRouter>
 			<div className="App">
-				<NavBar itemsAmount={selectedItems.length} />
+				<NavBar itemsAmount={selectedPhones.length} />
 				<Switch>
 					<Route path="/shop">
-						<Shop />
+						<Shop onClick={addPhoneToCart} />
 					</Route>
 					<Route path="/cart">
-						<Cart selectedItems={selectedItems} />
+						<Cart
+							selectedPhones={selectedPhones}
+							onChange={handleAmountChange}
+							onClick={removePhoneFormCart}
+						/>
 					</Route>
 					<Route path="/">
 						<Home />
